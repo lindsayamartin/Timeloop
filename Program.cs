49c28fcbf519da66469107804b0ddb8b3f327087
Lindsay <Lindsay@DESKTOP-MRT9GBF>
2021-09-03 13:56:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Timeloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            if (i < 1 || i > 100)
+            {
+                Console.WriteLine("Must be a number between 1 and 100");
+                return;
+            }
+            for (int x = 1; x < i + 1; x++)
+            {
+                Console.WriteLine(x + " Abracadabra");
+            }
+        }
+    }
+}
